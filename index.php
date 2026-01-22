0718348367673179d2b7b45a7b394a815b63d563
Jonas Nunes <suporte@jonasnunes.com.br>
2026-01-22 17:37:34 -0300

    Desenvolvendo a UI da área de login

--- index.php
+++ index.php
@@ -13,60 +13,66 @@ if(isset($_SESSION['id'])){
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="/assets/css/style.css">
+  <link rel="stylesheet" href="./assets/css/style.css">
+  <script src="/assets/js/script.js"></script>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+  <script src="https://kit.fontawesome.com/a325e67bc5.js" crossorigin="anonymous"></script>
   <title>Tela de Login</title>
 </head>
 <body>
-  <header>
-    <nav>
-      <svg></svg>
+  <div class="container">
+
+    <header class="header">
       <h4>Área de Login</h4>
-    </nav>
-  </header>
-
-  <div>
-    <svg></svg>
-    <h1>Olá, esse é um projeto de login</h1>
-    <p>Insira seus dados abaixo para efetuar o login ou realize o cadastro</p>
-  </div>
-
-  <main>
-    <?php 
-    
-      if(isset($_POST['email']) && isset($_POST['senha'])){
-        $email = $_POST['email'];
-        $senha = $_POST['senha'];
-
-        $sql = "SELECT * FROM contas WHERE email = :email";
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute([
-          'email' => $email
-        ]);
-
-        $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
-
-        if($usuario && password_verify($senha, $usuario['senha'])){
-          $_SESSION['id'] = $usuario['id'];
-          $_SESSION['email'] = $usuario['email'];
-          header("Location: dashboard.php");
-        } else {
-          echo "<p>Usuário ou senha incorretos</p>";
+    </header>
+  
+    <div class="content">
+      <i class="fa-solid fa-circle-user"></i>
+      <h1>Bem vindo de volta!</h1>
+      <p>Preencha os seus dados abaixo para logar. </p>
+    </div>
+  
+    <main>
+      <?php 
+      
+        if(isset($_POST['email']) && isset($_POST['senha'])){
+          $email = $_POST['email'];
+          $senha = $_POST['senha'];
+  
+          $sql = "SELECT * FROM contas WHERE email = :email";
+          $stmt = $pdo->prepare($sql);
+          $stmt->execute([
+            'email' => $email
+          ]);
+  
+          $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
+  
+          if($usuario && password_verify($senha, $usuario['senha'])){
+            $_SESSION['id'] = $usuario['id'];
+            $_SESSION['email'] = $usuario['email'];
+            header("Location: dashboard.php");
+          } else {
+            echo "<p>Usuário ou senha incorretos</p>";
+          }
         }
-      }
+      
+      ?>
+  
+      <form action="<?= $_SERVER['PHP_SELF']; ?>" method="post">
+        <label for="email">Digite seu email:</label>
+        <input type="text" name="email" placeholder="Email@dominio.com" required>
+        <label for="senha">Senha:</label>
+        <input type="password" name="senha" placeholder="Digite sua senha" required>
+        <button type="submit">Entrar</button>
+      </form>
+    </main>
+  
+    <div class="footer">
+      <p>Não tem conta? <a href="cadastro.php">Cadastre-se agora!</a></p>
+    </div>
     
-    ?>
-
-    <form action="<?= $_SERVER['PHP_SELF']; ?>" method="post">
-      <label for="email">Digite seu email</label>
-      <input type="text" name="email" placeholder="Email" required>
-      <label for="senha">Digite sua senha</label>
-      <input type="password" name="senha" placeholder="Senha" required>
-      <button type="submit">Entrar</button>
-    </form>
-  </main>
-
-  <div>
-    <p>Não tem conta? <a href="cadastro.php">Cadastre-se agora!</a></p>
   </div>
 
 </body>
